b8602801d8e2940bab89005a8f5eb7efac2f6b82
gabrielnotong <gabrielnotong@gmail.com>
2020-04-22 16:07:36 +0200

    Ad images and new migrations added

--- apps/rbnb/src/DataFixtures/AppFixtures.php
+++ apps/rbnb/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -13,18 +14,25 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
-        $slugFactory = Slugify::create();
         for ($i=0;$i<30;$i++) {
             $ad = new Ad();
             $content = '<p>' . join('<p></p>', $faker->paragraphs(5)) . '</p>';
             $title = $faker->sentence(2);
             $ad->setTitle($title)
-                ->setSlug($slugFactory->slugify($title))
                 ->setCoverImage($faker->imageUrl(1000, 300))
                 ->setIntroduction($faker->paragraph(2))
                 ->setContent($content)
                 ->setRooms(mt_rand(1,5))
                 ->setPrice(mt_rand(25, 99));
+
+            for($k = 0; $k <= mt_rand(2, 5); $k++) {
+                $image = new Image();
+                $image->setUrl($faker->imageUrl(1000, 300))
+                    ->setCaption($faker->sentence(2))
+                    ->setAd($ad);
+
+                $manager->persist($image);
+            }
 
             $manager->persist($ad);
         }
--- apps/rbnb/src/Entity/Ad.php
+++ apps/rbnb/src/Entity/Ad.php
@@ -2,10 +2,14 @@
 
 namespace App\Entity;
 
+use Cocur\Slugify\Slugify;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AdRepository")
+ * @ORM\HasLifecycleCallbacks
  */
 class Ad
 {
@@ -50,6 +54,27 @@ class Ad
      * @ORM\Column(type="integer")
      */
     private ?int $rooms;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Image", mappedBy="ad", orphanRemoval=true)
+     */
+    private Collection $images;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function initializeSlug(): void
+    {
+        if (empty($this->slug)) {
+            $this->slug = (Slugify::create())->slugify($this->title);
+        }
+    }
 
     public function getId(): ?int
     {
@@ -136,6 +161,37 @@ class Ad
     public function setRooms(int $rooms): self
     {
         $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setAd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->contains($image)) {
+            $this->images->removeElement($image);
+            // set the owning side to null (unless already changed)
+            if ($image->getAd() === $this) {
+                $image->setAd(null);
+            }
+        }
 
         return $this;
     }
