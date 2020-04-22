c6620176bbb537aa12d26d4fdddf18ab65f5a76f
gabrielnotong <gabrielnotong@gmail.com>
2020-04-22 18:37:04 +0200

    automatic param converter. show ad

--- apps/rbnb/src/Controller/AdController.php
+++ apps/rbnb/src/Controller/AdController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,14 +24,13 @@ class AdController extends AbstractController
 
     /**
      * @Route("/ads/{slug}", name="ads_show")
-     * @param string $slug
-     * @param AdRepository $adRepository
+     * @param Ad $ad
      * @return Response
      */
-    public function show(string $slug, AdRepository $adRepository)
+    public function show(Ad $ad)
     {
         return $this->render('ad/show.html.twig', [
-            'ad' => $adRepository->findOneBy(['slug' => $slug]),
+            'ad' => $ad,
         ]);
     }
 }
