29e93f53cc3a6661c27c1315a266469fd94d93cc
gabrielnotong <gabrielnotong@gmail.com>
2020-04-23 14:03:52 +0200

    User Authentication: User entity, migration, fixtures(+avatar) added

--- apps/rbnb/src/DataFixtures/AppFixtures.php
+++ apps/rbnb/src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use App\Entity\Image;
+use App\Entity\User;
 use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -14,16 +15,47 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
+        $users = [];
+        $genders = ['male', 'female'];
+
+        // Manage fake users
+        for($i = 1; $i <= 10; $i++) {
+            $user = new User();
+
+            $gender = $faker->randomElement($genders);
+
+            $picture = 'https://randomuser.me/api/portraits/';
+            $pictureId = $faker->numberBetween(1, 99) . '.jpg';
+
+            $picture .= ($gender == 'male' ? 'men/' : 'women/') . $pictureId;
+
+            $user->setFirstName($faker->firstName($gender))
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setIntroduction($faker->sentence)
+                ->setDescription('<p>' . join('<p></p>', $faker->paragraphs(1)) . '</p>')
+                ->setPicture($picture)
+                ->setHash('password');
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Manage Fake Ads
         for ($i=0;$i<30;$i++) {
             $ad = new Ad();
             $content = '<p>' . join('<p></p>', $faker->paragraphs(5)) . '</p>';
             $title = $faker->sentence(2);
+
+            $user = $users[mt_rand(0, count($users) -1)];
+
             $ad->setTitle($title)
                 ->setCoverImage("http://placehold.it/1000x400")
                 ->setIntroduction($faker->paragraph(2))
                 ->setContent($content)
                 ->setRooms(mt_rand(1,5))
-                ->setPrice(mt_rand(25, 99));
+                ->setPrice(mt_rand(25, 99))
+                ->setAuthor($user);
 
             for($k = 0; $k <= mt_rand(2, 5); $k++) {
                 $image = new Image();
---
+++ apps/rbnb/src/Entity/User.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace App\Entity;
+
+use Cocur\Slugify\Slugify;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\HasLifecycleCallbacks
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $picture;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $hash;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $introduction;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $slug;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Ad", mappedBy="author")
+     */
+    private Collection $ads;
+
+    public function __construct()
+    {
+        $this->ads = new ArrayCollection();
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function initializeSlug(): void
+    {
+        if (empty($this->slug)) {
+            $this->slug = (Slugify::create())->slugify($this->firstName . ' ' . $this->lastName);
+        }
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getHash(): ?string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    public function getIntroduction(): ?string
+    {
+        return $this->introduction;
+    }
+
+    public function setIntroduction(string $introduction): self
+    {
+        $this->introduction = $introduction;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Ad[]
+     */
+    public function getAds(): Collection
+    {
+        return $this->ads;
+    }
+
+    public function addAd(Ad $ad): self
+    {
+        if (!$this->ads->contains($ad)) {
+            $this->ads[] = $ad;
+            $ad->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAd(Ad $ad): self
+    {
+        if ($this->ads->contains($ad)) {
+            $this->ads->removeElement($ad);
+            // set the owning side to null (unless already changed)
+            if ($ad->getAuthor() === $this) {
+                $ad->setAuthor(null);
+            }
+        }
+
+        return $this;
+    }
+}
