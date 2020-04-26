f42d096817b1c0fc04cf00791de8a74e5d2eb77f
gabrielnotong <gabrielnotong@gmail.com>
2020-04-26 03:49:33 +0200

    adding datepicker to the form, Not available Dates disabled on date picker, days behind booking day are disabled, using datatransformer to allow symfony to validate dates

--- apps/rbnb/src/Form/BookingType.php
+++ apps/rbnb/src/Form/BookingType.php
@@ -3,32 +3,42 @@
 namespace App\Form;
 
 use App\Entity\Booking;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use App\Form\DataTransformer\StringToDateTimeTransformer;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BookingType extends ApplicationType
 {
+    private StringToDateTimeTransformer $transformer;
+
+    public function __construct(StringToDateTimeTransformer $transformer)
+    {
+        $this->transformer = $transformer;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add(
                 'startDate',
-                DateType::class,
-                $this->getConfiguration('From', 'Enter the arrival date', ['widget' => 'single_text']),
+                TextType::class,
+                $this->getConfiguration('From', 'Enter the arrival date'),
             )
             ->add(
                 'endDate',
-                DateType::class,
-                $this->getConfiguration('To', 'Enter the departure date', ['widget' => 'single_text']),
+                TextType::class,
+                $this->getConfiguration('To', 'Enter the departure date'),
             )
             ->add(
                 'comment',
                 TextareaType::class,
                 $this->getConfiguration('Comment', 'If you have any wish let us know about it...', ['required' => false]),
-            )
-        ;
+            );
+
+        $builder->get('startDate')->addModelTransformer($this->transformer);
+        $builder->get('endDate')->addModelTransformer($this->transformer);
     }
 
     public function configureOptions(OptionsResolver $resolver)
---
+++ apps/rbnb/src/Form/DataTransformer/StringToDateTimeTransformer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class StringToDateTimeTransformer implements DataTransformerInterface
+{
+    /**
+     * Gets DateTime data from symfony form and turn it into string
+     * @param \DateTimeInterface $dateTime
+     * @return string
+     */
+    public function transform($dateTime): string
+    {
+        if (!$dateTime) {
+            return '';
+        }
+
+        return $dateTime->format('Y/m/d');
+    }
+
+    /**
+     * Getting data from the form and transform it into what symfony expects
+     * @param string $strDate
+     * @return \DateTimeInterface
+     */
+    public function reverseTransform($strDate): \DateTimeInterface
+    {
+        if ($strDate === '') {
+            throw new TransformationFailedException('You must provide a date');
+        }
+
+        $date = \DateTime::createFromFormat('Y/m/d', $strDate);
+
+        if (!$date) {
+            throw new TransformationFailedException('invalid date format');
+        }
+
+        return $date;
+    }
+}
