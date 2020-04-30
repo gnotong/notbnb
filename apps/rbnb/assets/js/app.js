52af29b5a6e3dd12c216ee9eb9c8a02ade93bf54
gabrielnotong <gabrielnotong@gmail.com>
2020-04-30 05:10:25 +0200

    WebpackEncore: installation sf5.0.7, configuration and making jquery available for the entire application

---
+++ apps/rbnb/assets/js/app.js
@@ -0,0 +1,17 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.scss';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+import $ from 'jquery'; // installed using 'yarn add jquery --save-dev --ignore-engines'
+
+global.$ = global.jQuery = $; // this makes jquery available for the entire application.
+
+import 'bootstrap'; // installed using 'yarn add bootstrap --save-dev --ignore-engines'
+import 'bootstrap-datepicker'; // installed using 'yarn add bootstrap-datepicker --save-dev --ignore-engines'
